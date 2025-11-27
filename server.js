d0fe2d90ccecaf17ee87706d1b5d2809ecbf1d2a
Mateus Alcantara <159542239+teuzinn46@users.noreply.github.com>
2025-11-27 14:47:52 -0300

    correções de caminhos

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/sounds", express.static(path.join(__dirname, "public/sounds")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(3000, () => {
